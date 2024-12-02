42b0101323749c1a943110dc4dba2e20254e5a2e
Ezequiel Bomfim <ezequiel88er@gmail.com>
2024-12-02 14:41:19 -0300

    adicionando novos arquivos

---
+++ src/js/index.js
@@ -0,0 +1,12 @@
+var nomePessoa = prompt("Digite seu nome:")
+
+function incentivarPessoa(nomePessoa){
+    alert('Seja muito bem vindo ao nosso site, ' + nomePessoa+ '!')
+} 
+
+incentivarPessoa(nomePessoa)
+
+
+
+
+
